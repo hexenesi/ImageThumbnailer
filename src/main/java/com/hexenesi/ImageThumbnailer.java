4335f0054631527a4b89157fa3ce098bdb8960d6
Juanpcazarez <juanpcazarez@gmail.com>
2017-01-04 23:16:19 -0600

    modified the costructor of Scaler, so it should be empty
    Now must set the file to scale.

--- src/main/java/com/hexenesi/ImageThumbnailer.java
+++ src/main/java/com/hexenesi/ImageThumbnailer.java
@@ -6,9 +6,9 @@ public class ImageThumbnailer {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+        Scaler s=new Scaler();
         for (String arg : args) {
-            Scaler s = new Scaler(arg,"output.jpg","output_thumb.png");
+            s.setFile(arg);
             s.doScaling();
         }
     }
